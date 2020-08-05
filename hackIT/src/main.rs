59795cf52762b15df412c0664a6fc7179aa7d209
Joakim <joakimanderlind@gmail.com>
2020-08-05 13:59:36 +0200

    Add templating

--- hackIT/src/main.rs
+++ hackIT/src/main.rs
@@ -1,11 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+
+    let mut context = HashMap::new();
+    context.insert("name","yoda");
+    Template::render("index",&context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+	.attach(Template::fairing())
+	.mount("/", routes![index]).launch();
 }
+
